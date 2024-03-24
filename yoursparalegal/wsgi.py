f19084c56941fffe9b2b5816b28ab255684d48c4
Ansari Majid Ali <taznoboys@gmail.com>
2024-03-24 06:51:36 +0530

    Update wsgi.py

--- yoursparalegal/wsgi.py
+++ yoursparalegal/wsgi.py
@@ -13,6 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yoursparalegal.settings")
 
-application = get_wsgi_application()
-
-app = application
+app = get_wsgi_application()
